cf3918320f0960efd2959a6b6dc81bcf2aaf0a69
Alwan <alwanziryab@msn.com>
2019-09-17 12:43:01 +0200

    first mutation + data save

--- evoman_framework-master/try_alwan.py
+++ evoman_framework-master/try_alwan.py
@@ -8,18 +8,47 @@ experiment_name = 'dummy_demo'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
-# initializes environment with ai player using random controller, playing against static enemy
-env = Environment(experiment_name=experiment_name,
-				  playermode="ai",
-				  player_controller=player_controller(),
-			  	  speed="normal",
-				  enemymode="static",
-				  level=2)
+# # initializes environment with ai player using random controller, playing against static enemy
+# env = Environment(experiment_name=experiment_name,
+# 				  playermode="ai",
+# 				  player_controller=player_controller(),
+# 			  	  speed="normal",
+# 				  enemymode="static",
+# 				  level=2)
 
-n_hidden = env.player_controller.n_hidden[0]
-n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 # multilayer with 10 hidden neurons
+# n_hidden = env.player_controller.n_hidden[0]
+# n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 # multilayer with 10 hidden neurons
 
-n = np.zeros(n_vars)+0.5
+n = np.zeros(n_vars) + 0.5
 
-a = env.play(pcont=n)
+def fitness(kid):
+	f, pl, el, gt = env.play(pcont=kid)
+	return f
 
+fitness(n)
+
+# def mutation_N_weigths_max_mutate(kid, weigths_c, max_mutate):
+# 	count =0
+# 	weigths_c = int(weigths_c)
+# 	## can't loop using for...in, as choice returns new list
+# 	for i in np.random.choice(len(kid), weigths_c,replace=False):
+# 		count+=1
+# 		kid[i] += np.random.uniform(-max_mutate, max_mutate)
+# 		if kid[i] > 1:
+# 			kid[i] = 1
+
+# 		elif kid[i] < -1:
+# 			kid[i] = -1
+# 	return kid
+
+
+
+# def save_data(kid_fitness_list, save_file):
+# 	with open(save_file, 'a') as csvfile:
+# 		new_line = ','.join(str(kid_fitness) for kid_fitness in kid_fitness_list)
+# 		csvfile.write(new_line + '\n')
+
+
+# a = list(range(5))
+
+# save_data(a,'henk.txt')
